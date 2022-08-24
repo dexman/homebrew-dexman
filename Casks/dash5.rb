d43e325a6d4a439dd89ec09bb35b114dafc872cf
Arthur Dexter <adexter@dexman.net>
2022-08-24 06:12:06 -0500

    Add Dash 5

---
+++ Casks/dash5.rb
@@ -0,0 +1,11 @@
+cask "dash5" do
+  version "5.5.2"
+  sha256 "cd98b4496bfa82ff2cd4c98f7d8a35eb83b909b7da605eb291d12a0c162c24a8"
+
+  url "https://newyork.kapeli.com/downloads/v5/Dash.zip"
+  name "Dash 5"
+  desc "API Documentation Browser and Code Snippet Manager."
+  homepage "https://kapeli.com/dash"
+
+  app "Dash.app"
+end
