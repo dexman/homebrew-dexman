179c2a8a196070d41a5723a8fe9ddcad126dca06
Arthur Dexter <adexter@dexman.net>
2023-02-28 17:06:07 -0600

    Replace dash5 with dash6

--- Casks/dash6.rb
+++ Casks/dash6.rb
@@ -1,9 +1,9 @@
-cask "dash5" do
-  version "5.5.2"
+cask "dash6" do
+  version "6.4.0"
   sha256 "cd98b4496bfa82ff2cd4c98f7d8a35eb83b909b7da605eb291d12a0c162c24a8"
 
-  url "https://newyork.kapeli.com/downloads/v5/Dash.zip"
-  name "Dash 5"
+  url "https://sanfrancisco.kapeli.com/downloads/v6/Dash.zip"
+  name "Dash 6"
   desc "API Documentation Browser and Code Snippet Manager."
   homepage "https://kapeli.com/dash"
 
